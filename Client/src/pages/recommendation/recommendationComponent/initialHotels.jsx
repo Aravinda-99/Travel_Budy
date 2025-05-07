2290917db00768bd9e4e11dddded25a3ad6cc5da
Aravinda Viraj <Aravindaviraj99@gmail.com>
2025-05-07 18:11:14 +0530

    create moke data fille

---
+++ Client/src/pages/recommendation/recommendationComponent/initialHotels.jsx
@@ -0,0 +1,69 @@
+export const initialHotels = [
+    {
+      id: '1',
+      name: 'Grand Seaside Resort',
+      image: 'https://images.pexels.com/photos/338504/pexels-photo-338504.jpeg',
+      city: 'Miami',
+      country: 'USA',
+      priceRange: '$$$',
+      amenities: ['Pool', 'Spa', 'WiFi', 'Restaurant', 'Gym'],
+      userRating: 5,
+      affiliateBookingLink: 'https://example.com/book-grand-seaside'
+    },
+    {
+      id: '2',
+      name: 'Mountain View Lodge',
+      image: 'https://images.pexels.com/photos/1001965/pexels-photo-1001965.jpeg',
+      city: 'Aspen',
+      country: 'USA',
+      priceRange: '$$$$',
+      amenities: ['Ski Access', 'Fireplace', 'Restaurant', 'Spa', 'Concierge'],
+      userRating: 4,
+      affiliateBookingLink: 'https://example.com/book-mountain-view'
+    },
+    {
+      id: '3',
+      name: 'Urban Boutique Hotel',
+      image: 'https://images.pexels.com/photos/1838554/pexels-photo-1838554.jpeg',
+      city: 'London',
+      country: 'UK',
+      priceRange: '$$',
+      amenities: ['WiFi', 'Breakfast', 'Bar', '24hr Reception'],
+      userRating: 4,
+      affiliateBookingLink: 'https://example.com/book-urban-boutique'
+    },
+    {
+      id: '4',
+      name: 'Urban Boutique Hotel',
+      image: 'https://images.pexels.com/photos/1838554/pexels-photo-1838554.jpeg',
+      city: 'London',
+      country: 'UK',
+      priceRange: '$$',
+      amenities: ['WiFi', 'Breakfast', 'Bar', '24hr Reception'],
+      userRating: 4,
+      affiliateBookingLink: 'https://example.com/book-urban-boutique'
+    },
+    {
+      id: '5',
+      name: 'Mountain View Lodge',
+      image: 'https://images.pexels.com/photos/1001965/pexels-photo-1001965.jpeg',
+      city: 'Aspen',
+      country: 'USA',
+      priceRange: '$$$$',
+      amenities: ['Ski Access', 'Fireplace', 'Restaurant', 'Spa', 'Concierge'],
+      userRating: 4,
+      affiliateBookingLink: 'https://example.com/book-mountain-view'
+    },
+    {
+      id: '1',
+      name: 'Grand Seaside Resort',
+      image: 'https://images.pexels.com/photos/338504/pexels-photo-338504.jpeg',
+      city: 'Miami',
+      country: 'USA',
+      priceRange: '$$$',
+      amenities: ['Pool', 'Spa', 'WiFi', 'Restaurant', 'Gym'],
+      userRating: 5,
+      affiliateBookingLink: 'https://example.com/book-grand-seaside'
+    }
+    
+  ];
