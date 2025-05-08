fa10cc1d8b6d7929b92ce434b744aa9c6fa2142a
Aravinda Viraj <Aravindaviraj99@gmail.com>
2025-05-08 12:22:41 +0530

    create table for get hotel backend details

--- Client/src/pages/Profile/UserProfilePage.jsx
+++ Client/src/pages/Profile/UserProfilePage.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import UserDetails from '../Profile/ProfileComponent/UserDetails.jsx';
 import { mockUser } from '../Profile/ProfileComponent/mockUser.jsx';
+import HotelTable from '../recommendation/recommendationComponent/hotelTable.jsx';
 
 const UserProfilePage = () => {
   return (
    
       <div className="max-w-7xl mx-auto px-4 py-8 pt-20">
           <UserDetails user={mockUser} />
+
+          <div className="mt-8">
+            <HotelTable />
+          </div>
+          
       </div>
 
   );
